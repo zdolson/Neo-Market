9049575cfc90e52919847671878283dad40c8be7
Zachary Olson <zdolson@ucsc.edu>
2018-02-25 12:44:36 -0800

    tidied up boilerplate backend code with certain modificaitons to work on my (Zack's) local privnet

---
+++ backend/backend/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+
+    /*
+     * scriptHash is your smart contract Script Hash, without the first 2 '0x' characters
+     * To retrieve your Script Hash: (in neo-gui: right click smart contract address > View Contract)
+     */
+     scriptHash: '1e20b24de4e698abc174ca9cf0be09a394abaf06',
+
+    /*
+     * RESTEndpoint is an endpoint to a neon-wallet-db REST server that's connected to your desired net (MainNet, TestNet, PrivNet)
+     * MainNet: http://api.wallet.cityofzion.io
+     * TestNet: http://testnet-api.wallet.cityofzion.io
+     * PrivNet: HTTP URL to your neon-wallet-db instance IP with port 5000
+     */
+     //RESTEndpoint: 'http://testnet-api.wallet.cityofzion.io',
+     RESTEndpoint: 'http://neo-privnet:5000',
+
+     wif: 'KxDgvEKzgSBPPfuVfw67oPQBSjidEiqTHURKSDL1R7yGaGYAeYnr',
+
+};
