dec8c6a67803f17d342995b266e2c913322d4585
TheFoodGuy <dliang5@ucsc.edu>
2018-02-28 02:00:53 -0800

    dam

---
+++ backend/Contracts/tiredofthis.sh
@@ -0,0 +1,11 @@
+# alright this is the fucking file to get everything you fucking need in one go with docker 
+# because i can't be asked to delete privnet all the time cuz im lazy 
+# alright: run this chmod 777 tiredofthis.sh 
+# ./tiredofthis.sh that's fucking it 
+
+apt-get update 
+apt-get install vim 
+
+curl 
+curl 
+
