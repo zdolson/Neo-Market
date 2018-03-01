19d9b54734e6dc3a0f28160ab6393ef10a54125d
TheFoodGuy <dliang5@ucsc.edu>
2018-02-28 16:12:41 -0800

    pushing some files to make it easier for me

--- backend/Contracts/tiredofthis.sh
+++ backend/Contracts/tiredofthis.sh
@@ -6,8 +6,8 @@
 apt-get update 
 apt-get install vim 
 
-curl https://github.com/zdolson/Neo-Market/blob/master/backend/Contracts/testing.py -o testing.py
-curl https://github.com/zdolson/Neo-Market/blob/master/backend/Contracts/compile.py -o compile.py
+curl https://raw.githubusercontent.com/zdolson/Neo-Market/master/backend/Contracts/testing.py -o testing.py
+curl https://raw.githubusercontent.com/zdolson/Neo-Market/master/backend/Contracts/compile.py -o compile.py
 curl https://s3.amazonaws.com/neo-experiments/neo-privnet-old.wallet -o w1.wallet
 
 ### make sure to only curl this file and then run it and it'll do mostly everything for you tho no idea 
