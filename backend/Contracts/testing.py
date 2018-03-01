8a2939018864e839c0cd5ace23faf9c0b6316485
TheFoodGuy <dliang5@ucsc.edu>
2018-02-28 20:46:03 -0800

    slowly building up

--- backend/Contracts/testing.py
+++ backend/Contracts/testing.py
@@ -1,7 +1,7 @@
 """ my god this is difficult """
 
 from boa.blockchain.vm.Neo.Storage import GetContext, Put, Delete, Get
-
+from post import init_Post
 
 def register(name, addr): 
     a = Get(GetContext, name) 
@@ -22,6 +22,17 @@ def isdone(name, addr):
         print("does exist you idiot ")
         return False
 
+def getclass(owner, title, desc, price, amount):
+    post = init_Post(owner, title, desc, price, amount)
+    print("this is the owner") 
+    print(post.owner)
+    print("im selling this shit") 
+    print(post.title, post.desc)
+    print("for this much : ") 
+    print(post.price, post.amount)
+    print("GOING TO SLEEP BOYS")
+    return True # PLEASE WORK SO I CAN NAP
+
 """ This is going to be so fucking barebone that it's bare """ 
 
 # main fucking issue: I get list error what the flying fuck is it? 
@@ -39,6 +50,10 @@ def Main(operation, args):
         print("after you fucking what?")
     elif operation == "isdone":
         print("why the fuck are you here, push when you get this down")
+        isdone(b,a) 
+    elif operation == "getclass": 
+        print("getting this fucking class") 
+        getclass(args[0], args[1], args[2], args[3], args[4])
     else:
         print("fucking run shithead") 
     print("done") 
