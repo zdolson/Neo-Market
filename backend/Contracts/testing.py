1dff1c4690afabca230993094a9a4feafdd8c87c
TheFoodGuy <dliang5@ucsc.edu>
2018-03-01 13:09:21 -0800

    updating and changing the language of it

--- backend/Contracts/testing.py
+++ backend/Contracts/testing.py
@@ -3,35 +3,46 @@
 from boa.blockchain.vm.Neo.Storage import GetContext, Put, Delete, Get
 from post import init_Post
 
+# this registers a user to their addr in the ocntract
+# then it registers their address to a list of list 
 def register(name, addr): 
     a = Get(GetContext, name) 
-    print("alright, here i am now in register") 
+    print("checking if user exist") 
     if not a: 
-        print("im not fucking there you fucking shit ass head") 
+        print("user does not exist - registering") 
         Put(GetContext, name, addr)
     else: 
-        print("who you fucking checking out ya fucker") 
+        print("user already exist") 
     return True
 
+# this checks if the user is registered or not
 def isregister(name, addr):
     a = Get(GetContext, name) 
     if not a:
-        print("he doesn't fucking exist") 
+        print("there is no user in contract") 
         return True 
     else: 
-        print("does exist you idiot ")
+        print("user is in contract")
         return False
 
-def buy(buyerAddr, sellerAddr): 
+# this uses the buyer's address to purchase the address of the seller 
+# perhaps the item of the seller and the amount of it
+def buy(buyerAddr, sellerAddr, title, amount): 
     pass
 
+
+# this creates the posting and appens that list of params 
+# to the list of list (backlog) at the moment
 def createPost(owner, title, desc, price, amount): 
     pass 
 
+# this is a getter for createPost and gets the selected list and returns it 
+# more descriptions to come *backlog*
 def getPost(owner, title): 
     pass 
 
-    
+# this is to check if it was possible to build a class in neo-python and 
+# return true if it does, that's it 
 def getclass(owner, title, desc, price, amount):
     post = init_Post(owner, title, desc, price, amount)
     print("this is the owner") 
@@ -43,32 +54,40 @@ def getclass(owner, title, desc, price, amount):
     print("GOING TO SLEEP BOYS")
     return True # PLEASE WORK SO I CAN NAP
 
-""" This is going to be so fucking barebone that it's bare """ 
 
-# main fucking issue: I get list error what the flying fuck is it? 
-# no idea: I believe im not calling (operation, args) right but i followed fucking dean shit head 
-# so debugging all the way down
+""" the list of operations the file will run in
+    1. register
+    2. isregister
+    3. buy 
+    4. createPost
+    5. getPost
+    6. getclass
+
+    Until they are functionally good then we'll separate them into separate
+    files to make this code look nicer 
+
+ """
 def Main(operation, args):
-    print("does this fucking work first?")
+    print("starting")
     if operation == "register":
         a = args[0]
         b = args[1]
-        print("you fucking what?")
-        register(b, a) 
-        print("after you fucking what?")
+        print("in op - register")
+        register(b, a)
     elif operation == "isregister":
         a = args[0]
         b = args[1]
-        print("why the fuck are you here, push when you get this down")
+        print("in op - isRegister")
         isregister(b,a) 
     elif operation == "getclass": 
         a = args[0]
         b = args[1]
-        print("getting this fucking class") 
+        print("in op - getClass") 
         c = args[2]
         d = args[3] 
         e = args[4]
         getclass(a, b, c, d, e)
     else:
-        print("fucking run shithead") 
+        print("no op exist - ")
+        print(operation) 
     print("done") 
