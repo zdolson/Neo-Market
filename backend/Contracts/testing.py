ea915d2e93328522b7f891281997e564e5326cb2
TheFoodGuy <dliang5@ucsc.edu>
2018-02-28 19:31:17 -0800

    slowly building up

--- backend/Contracts/testing.py
+++ backend/Contracts/testing.py
@@ -3,19 +3,35 @@
 from boa.blockchain.vm.Neo.Storage import GetContext, Put, Delete, Get
 
 
+def register(name, addr): 
+    a = Get(GetContext, name) 
+    print("alright, here i am now in register") 
+    if not a: 
+        print("im not fucking there you fucking shit ass head") 
+    else: 
+        print("who you fucking checking out ya fucker") 
+    return True
+
+
+
 """ This is going to be so fucking barebone that it's bare """ 
 
 # main fucking issue: I get list error what the flying fuck is it? 
 # no idea: I believe im not calling (operation, args) right but i followed fucking dean shit head 
 # so debugging all the way down
-def Main(operation): 
+def Main(operation, args):
     print("does this fucking work first?")
+    a = args[0]
+    b = args[1]
+    print("i fucking got it please")
+    print(a) 
+    print(b) 
     if operation == "register":
         print("you fucking what?")
-        return True 
+        register(b, a) 
+        print("after you fucking what?")
     elif operation == "isdone":
         print("why the fuck are you here, push when you get this down")
-        return True
     else:
         print("fucking run shithead") 
-        return True 
+    print("done") 
