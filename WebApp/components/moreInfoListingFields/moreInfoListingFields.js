0343a053940e21dbc9eec70c7a8087fa3a7bbb9a
nichc25 <nccheung@ucsc.edu>
2018-03-08 16:33:02 -0800

    Forgot to add one more file change

--- WebApp/components/moreInfoListingFields/moreInfoListingFields.js
+++ WebApp/components/moreInfoListingFields/moreInfoListingFields.js
@@ -33,33 +33,37 @@ class MoreInfoListingFields extends Component {
   render () {
     return (
       <div className='MoreInfoListingFields'>
-      <Stylesheet sheet={sheet} />
+        <Stylesheet sheet={sheet} />
         <div className='moreInfoListingTitle'>
           <h2>Test Item 1</h2>
-          <div className ='stuffies'>
-            <p>Shipping and Handling Info Here.</p>
-            <p>Ship Anywhere In the US and Canada!</p>
-          </div>
+            <div className ='stuffies'>
+              <p>Shipping and Handling Info Here.</p>
+              <p>Ship Anywhere In the US and Canada!</p>
+            </div>
+        </div>
+
         <div className='moreInfoListingPicture'>
           <Galaxy className="galaxy"/>
           <Stylesheet sheet={sheet} />
         </div>
 
-        <div class='moreInfoListingDescription'>
-        <h2>Selling My Grumpy Cat! Near-to-mint condition!</h2>
-        <p>Ten the hastened steepest feelings pleasant few surprise property. An brother he do colonel against minutes uncivil. Can how elinor warmly mrs basket marked. Led raising expense yet demesne weather musical. Me mr what park next busy ever. Elinor her his secure far twenty eat object. Late any far saw size want man. Which way you wrong add shall one. As guest right of he scale these. Horses nearer oh elinor of denote.</p>
-        </div>
         <div className='moreInfoListingDescription'>
-
+          <h2>Selling My Grumpy Cat! Near-to-mint condition!</h2>
+          <p>Ten the hastened steepest feelings pleasant few surprise property. An brother he do colonel against minutes uncivil. Can how elinor warmly mrs basket marked. Led raising expense yet demesne weather musical. Me mr what park next busy ever. Elinor her his secure far twenty eat object. Late any far saw size want man. Which way you wrong add shall one. As guest right of he scale these. Horses nearer oh elinor of denote.</p>
+        </div>
+      
+        <div className='moreInfoListingDescription'>
+          <h2> Description </h2>
         </div>
 
-        <div class='moreInfoListingSeller'>
+        <div className='moreInfoListingSeller'>
           <SellerRating className="sellerRating"/>
           <h3>Seller A</h3>
         </div>
-        <div class='purchaseBtn' onClick={this.addItem}>
-          hello! click me to purchase this item!
 
+        <div className='purchaseBtn' onClick={this.addItem}>
+          hello! click me to purchase this item!
+        </div>
       </div>
     )
   }
