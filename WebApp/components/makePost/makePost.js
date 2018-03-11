fea464e1178b1067ca708759c3e0c7e1c9b19308
Alec Felt <allfelt@ucsc.edu>
2018-03-11 16:05:03 -0700

    post creation logic complete for dev

--- WebApp/components/makePost/makePost.js
+++ WebApp/components/makePost/makePost.js
@@ -19,6 +19,7 @@ import purchasesPage from '../purchasesPage/purchasesPage.js';
 import peoplePage from '../peoplePage/peoplePage.js';
 
 import MakePostForm from '../makePostForm/makePostForm.js'
+import MakePostFormNew from '../makePostForm/makePostFormNew.js'
 
 export class MakePost extends Component {
   constructor (props, context) {
@@ -32,7 +33,7 @@ export class MakePost extends Component {
     var addItem = this.props.addItem;
     return (
       <div className = "makePost">
-        <MakePostForm addItem={addItem}/>
+        <MakePostFormNew addItem={addItem}/>
         <Stylesheet sheet={sheet} />
       </div>
 
