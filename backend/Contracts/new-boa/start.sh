fa4556355b2fa1fddadd0fcef1db6a3c9ab24db2
David Liang <dliang5@ucsc.edu>
2018-03-11 22:44:56 -0700

    david's working on new compiler

---
+++ backend/Contracts/new-boa/start.sh
@@ -0,0 +1,9 @@
+#### how to run it
+# curl this file as raw from github then chmod777 it 
+# ./start.sh 
+
+apt-get update 
+apt-get install vim # cuz i like vim 
+apt-get install wget
+apt-get install nano # for easier copying ability 
+
