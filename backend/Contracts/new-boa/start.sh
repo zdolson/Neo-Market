0491bcd489373485d5c5bbca7c9ba2c9faf7a743
David Liang <dliang5@ucsc.edu>
2018-03-12 05:55:38 -0700

    new contract based on the compiler

---
+++ backend/Contracts/new-boa/start.sh
@@ -0,0 +1,13 @@
+#### how to run it
+# curl this file as raw from github then chmod777 it 
+# ./start.sh 
+
+apt-get update 
+apt-get install vim # cuz i like vim 
+apt-get install wget
+apt-get install nano # for easier copying ability 
+
+curl https://raw.githubusercontent.com/zdolson/Neo-Market/david_develop/backend/Contracts/new-boa/testing.py -o testing.py 
+curl https://raw.githubusercontent.com/zdolson/Neo-Market/david_develop/backend/Contracts/new-boa/compile.py -o compile.py
+curl https://s3.amazonaws.com/neo-experiments/neo-privnet.wallet -o w1.wallet
+curl https://raw.githubusercontent.com/zdolson/Neo-Market/david_develop/backend/Contracts/new-boa/serialize.py -o serialize.py 
