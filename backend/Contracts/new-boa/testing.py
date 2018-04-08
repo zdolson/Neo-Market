c90c60a0176b88bd4042e0114bd09a110f8fddde
David Liang <davidxliang@gmail.com>
2018-04-08 13:20:19 -0700

    deleted alot of useless files and updated comments -> onto cleaning up the functions

--- backend/Contracts/new-boa/testing.py
+++ backend/Contracts/new-boa/testing.py
@@ -1,76 +1,58 @@
-from boa.builtins import list, concat, range, concat, take
+from boa.builtins import list, concat, range, take
 from boa.interop.Neo.Storage import GetContext, Get, Put, Delete
-from serialize import serialize_array, serialize_var_length_item, deserialize_bytearray
-from boa.interop.Neo.Runtime import Log, Notify
-""" CHECKLIST:
-    1. make sure list of one item work - done
-    2. call same function and see if item is there and add another one to it - done
-    3. can you print the entire set of list? - TODO: DO LAST - no need to rush this one
-    4. can i select a certain index of the list? - ready up
+from boa.interop.Neo.Runtime import Log, Notify, Serialize, Deserialize
 
-    AFTER THAT REPORT BACK
-
-    1. can i create a post? - starting at 5:15
-    2. can i add another post to the list?
-    3. can i select an index of that post?
-    4. can i delete a post?
-    5. can i "buy" - NOTE cannot use other wallet so that's an issue leave that to zack
-
-    SHOULD BE DONE BY HERE
-    1. is there any bugs?
-    2. what are the test cases for this code?
-    3. can it be improved upon?
 """
-# splitting things up here - helper to create master
+@Function: fillMaster
+@Contributor: dliang 
+@Param: {string} name
+@Return: void 
+Purpose: for each register call, append the name to master list
+"""
 def fillMaster(name):
     masterbList = Get(GetContext(), '1')
-    masterList = deserialize_bytearray(masterbList)
+    masterList = Deserialize(masterbList)
     masterList.append(name)
     masterList.append(',')
-    masterbList = serialize_array(masterList)
+    masterbList = Serialize(masterList)
     Put(GetContext(), '1', masterbList)
-
-# params 'register' [username, addr]
+    
+"""
+@Function: register
+@Contributor: dliang 
+@Param: {list of strings} args 
+        1. {string} name
+        2. {string} address
+@Return: void by default otherwise return false if conflicts with masterList
+Purpose: This will register the user and their address to the smart contract 
+         and add the name to master list
+"""
 def register(args):
     if args[0] == '1':
-        Log("please no - this is for master list")
+        Log("This is only avaliable for MasterList")
         return False
     else:
         isMasterThere = Get(GetContext(), '1')
         if not isMasterThere:
             # create master here
-            masterList = serialize_array([])
+            masterList = Serialize([])
             Put(GetContext(), '1', masterList)
     print("in register")
-    print("after creation of list")
-    #b = list(length=2)
-    a = args[0] # no need to do this tbh
-    b = args[1]
-    print("allocating a and b")
-    bstuff = serialize_array([])
+    # a = args[0] # no need to do this tbh
+    # b = args[1]
+    # print("allocating a and b")
+    bstuff = Serialize([]) # allocating an empty list to the user's address
     print("done serializing array here")
-    Put(GetContext(), a, b)
-    Put(GetContext(), b, bstuff)
-    print("donnnnne")
+    Put(GetContext(), args[0], args[1])
+    Put(GetContext(), args[1], bstuff)
+    print("done registering")
     fillMaster(args[0]) # only want the names
 
-# params 'addone' [username, one_item]
-def addone(args):
-    addr = Get(GetContext(), args[0])
-    bstuff = Get(GetContext(), addr)
-    stuff = deserialize_bytearray(bstuff)
-    stuff.append(args[1])
-    stuff_length = len(stuff)
-    Log('new length of stuff:')
-    Log(stuff_length)
-    bstuff = serialize_array(stuff)
-    Put(GetContext(), addr, bstuff)
-    print("done with addone")
 # params 'createpost' [id, owner, title, desc, price, amount]
 def createpost(args):
     addr = Get(GetContext(), args[1])
     bList = Get(GetContext(), addr)
-    stuff = deserialize_bytearray(bList)
+    stuff = Deserialize(bList)
     stuff.append(args[0])
     stuff.append(',')
     stuff.append(args[1])
@@ -85,54 +67,44 @@ def createpost(args):
     stuff.append(';')
     Log('new length of stuff:')
     Log(len(stuff)) # this may break it here
-    bList = serialize_array(stuff)
+    bList = Serialize(stuff)
     Put(GetContext(), addr, bList)
     Log("printing post down there in the format")
 
-# params 'delete' [owner, index]
-def deletepost(args):
+"""
+@Function: select
+@Contributor: <whoever gets to it> 
+@Param: {list} args
+        1. {string} args[0] 
+@Return: void 
+Purpose: This is used to delete a post
+"""def deletepost(args):
     addr = Get(GetContext(), args[0])
     bList = Get(GetContext(), addr)
-    stuff = deserialize_bytearray(bList)
+    stuff = Deserialize(bList)
     stuff[args[1]] = "0"
-    bList = serialize_array(stuff)
+    bList = Serialize(stuff)
     Put(GetContext(), addr, bList)
     print("done with setting the post to 0")
 
-# params 'select' [username, index of item]
+"""
+@Function: select
+@Contributor: dliang 
+@Param: {list} args
+        1. {string} args[0] 
+@Return: void 
+Purpose: This is mainly used for testing cases but it uses the name and prints the first 
+         element or value the address of the name is holding.
+"""
 def select(args):
     addr = Get(GetContext(), args[0])
     # i got the addr now
     bList = Get(GetContext(), addr)
     # i got the byte array now - time to deserialize
-    stuff = deserialize_bytearray(bList)
+    stuff = Deserialize(bList)
     print("printing the value of the list now")
     print(stuff[args[1]])
     print("done with select")
-
-# # params 'getposts' [username] - will do multiple usernames as well
-# def getposts(args):
-#     for i in range(len(args)):
-#         addr = Get(GetContext(), args[i])
-#         bList = Get(GetContext(), addr)
-#         stuff = deserialize_bytearray(bList)
-#         for j in range(len(stuff)):
-#             Log(stuff[j])
-
-# # params 'getallposts' []
-# # no args please
-# def getallposts():
-#     masterbList = Get(GetContext(), '1')
-#     masterList = deserialize_bytearray(masterbList)
-#     for i in range(len(masterList)):
-#         Log(i) # this is the name of a registered person
-#         addr = Get(GetContext(), i)
-#         bList = Get(GetContext(), addr)
-#         stuff = deserialize_bytearray(bList)
-#         for j in range(len(stuff)):
-#             Log(j)
-#     ## should be done here
-#     Log("done getting all post")
 
 # params 'isregister' [username]
 def isregister(args):
@@ -144,30 +116,47 @@ def isregister(args):
         print("the user is registered")
         return True
 
-
+"""
+@Function: fillMaster
+@Contributor: Colin, dliang
+@Param: {string} operation 
+        {list} args
+@Return: boolean
+Purpose: Runs the smart contract and acts accordingly to the user and their respective args
+"""
 def Main(operation, args):
+    boolStatus = True # assuming it's true by default
     if operation == "register":
         print("register op - here")
+        if len(args) != 2:
+            Notify("wrong length of args") 
+            boolStatus = False
         register(args)
     elif operation == "isregister":
         print("checking if registered op")
+        if len(args) != 2:
+            Notify("wrong length of args")
+            boolStatus = False
         isregister(args)
     elif operation == "select":
         print("select op - here")
+        if len(args) != 2:
+            Notify("wrong length of args")
+            boolStatus = False
         select(args)
-    # elif operation == "getposts":
-    #     print("getpost op - here")
-    #     getposts(args)
-    elif operation == "addone":
-        print("addone op - here")
-        addone(args)
     elif operation == "createpost":
         print("creating a post - here")
+        if len(args) != 2:
+            Notify("wrong length of args")
+            boolStatus = False
         createpost(args)
     elif operation == "deletepost":
         print("deleting a post")
+        if len(args) != 2:
+            Notify("wrong length of args")
+            boolStatus = False
         deletepost(args)
     else:
-        print("what op?")
-        return False
-    return True
+        Notify("Bad operation name or it does not exist") 
+        boolStatus = False
+    return boolStatus
