04748b568eaea42365b25fe4bb8d5a8ee6dc14fc
nichc25 <nccheung@ucsc.edu>
2018-04-21 23:57:28 -0700

    Frontend Task - Adding script for adding multiple users to firebase database via admin

---
+++ WebApp/firebaseAdmin/firebaseAdminAddUsers.js
@@ -0,0 +1,94 @@
+/*
+  Title - firebaseAdminAddUsers.js
+  Author - Nicholas Cheung
+
+  This script gives the user the ability to add additional users using admin privileges.
+
+  To run this script you must pass in a text file to be read from command line
+  Example run:
+       node firebaseAdminAddUsers.js --read_json <text_file_name>.text
+
+  NOTES - When you pass in a text file please make sure that the json is formatted correctly and that there is no additional empty lines after the last json user. 
+        - If you need an example of a text file, there is a default text file with default users called - defaultUsers.text
+*/
+
+var admin = require('firebase-admin');
+
+var serviceAccount = require('../firebaseAdmin/credentialJSON.js');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://neo-market-8a303.firebaseio.com"
+});
+
+function grab(flag) {
+  // Function is for grabing command line arguements
+  var index = process.argv.indexOf(flag);
+  return (index === -1) ? null : process.argv[index+1]  
+
+}
+
+function isUserRegisterd(userName, userList) {
+  for(var i = 0; i<userList.length; i++) {
+    if(userName == userList[i]){
+      return true
+    }
+  }
+  return false
+}
+
+function setupFunction(){
+  var db = admin.database();
+  var rootRef = db.ref();
+  var promise = rootRef.child('/Users/').once('value', function(snapshot) {
+    if(!snapshot.exists()) {
+      console.log('There is no users section in the database, going to create one.')
+      admin.database().ref('/Users/' + 'placeholderUser').set('defaultUser')
+    }
+  });
+  return promise
+}
+
+function fireBaseAddUsers(){
+  var read_json = grab('--read_json');
+  var db = admin.database();
+  var ref = db.ref("/Users/");
+  
+  if (!read_json) {
+    console.log('Please pass a file for the script to read')
+  } else {
+    var fs = require('fs');
+
+    var readtext = fs.readFileSync(read_json, 'utf8');
+    var split_json = readtext.split('\n')
+
+    ref.once("value", function(snapshot) { 
+      for(var i = 0; i<Object.keys(split_json).length;i++) {
+        try {
+          var curr_user = JSON.parse(split_json[i])
+          var userNameList = Object.keys(snapshot.val())
+          if(!isUserRegisterd(curr_user['userName'], userNameList)){
+            admin.database().ref('/Users/' + curr_user['userName']).set(curr_user);
+            console.log('The user ' + curr_user['userName'] + ' was added to the database')
+          } else {
+            console.log('The user ' + curr_user['userName'] + ' is already in the database. Therefore, skipping adding this user.')
+          }
+        } catch(err) {
+          console.log('There was a problem parsing the JSON, please double check that the user json is formatted correctly.')
+          console.log(err)
+        }
+      }
+
+      if(snapshot.child('placeholderUser').val() != null){
+        admin.database().ref('/Users/placeholderUser/').remove()
+      }
+    });
+  }
+}
+
+function main() {
+  return setupFunction().then(() => fireBaseAddUsers());
+}
+
+main();
+
