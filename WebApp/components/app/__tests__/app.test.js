f93f1cce933189bbd06f34a7b600914f8987e5b2
Alec Felt <allfelt@ucsc.edu>
2018-04-30 12:31:57 -0700

    added all unit tests for app.js component

--- WebApp/components/app/__tests__/app.test.js
+++ WebApp/components/app/__tests__/app.test.js
@@ -3,22 +3,43 @@ import {shallow} from 'enzyme'
 import App from '../app.js'
 import {HashRouter} from 'react-router-dom'
 import * as firebase from "firebase"
-import {loginUser, initializeApp} from '../../../fireBaseFunctions.js'
+import {loginUser, initializeApp} from '../../fireBaseFunctions.js'
+
+import RightSideBar from '../../rightSideBar/rightSideBar.js'
+import RoutingComponent from '../../routingComponent/routingComponent.js'
 
 describe('App Component', () => {
 
+  // Firebase initialization
   initializeApp();
   loginUser('allfelt@ucsc.edu', 'allfelt');
 
-  .catch( (err) => {
-    console.error(err);
-  });
+  const wrapper = shallow(<App/>);
 
   it('renders one child', () => {
-    const wrapper = shallow(<App/>);
-
     let expected = 1;
     let actual = wrapper.length;
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('renders 7 component predecessors', () => {
+    let expected = 7;
+    let actual = wrapper.childAt(0).children().length;
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('passes RightSideBar 5 props', () => {
+    let expected = 5;
+    let actual = Object.keys(wrapper.find(RightSideBar).props()).length;
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('passes RoutingComponent 8 props', () => {
+    let expected = 8;
+    let actual = Object.keys(wrapper.find(RoutingComponent).props()).length;
 
     expect(actual).toEqual(expected);
   });
