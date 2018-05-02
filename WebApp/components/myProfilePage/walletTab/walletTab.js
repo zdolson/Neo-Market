b76f9420b24b15574d2f44c7b273e5206df8b873
Alec Felt <allfelt@ucsc.edu>
2018-05-01 22:01:44 -0700

    finished basic styling of WalletTab, added placeholder image

--- WebApp/components/myProfilePage/walletTab/walletTab.js
+++ WebApp/components/myProfilePage/walletTab/walletTab.js
@@ -3,6 +3,7 @@ import {Stylesheet} from '../../stylesheet.js'
 import sheet from './walletTab.scss'
 
 import ImportPhotoIcon from '../../assets/ImportPhotoIcon.svg'
+import ProfilePhoto from '../../assets/DSC_0046.jpg'
 
 class WalletTab extends Component {
   constructor(props) {
@@ -18,9 +19,9 @@ class WalletTab extends Component {
           <div className="photoImportName">
             <div className="photoName">
               <div className="photo">
-                photo
+                <img src={ProfilePhoto} width="150" />
               </div>
-              <div className="name">
+              <div className="userName">
                 name
               </div>
             </div>
@@ -32,7 +33,23 @@ class WalletTab extends Component {
 
         <div className="topBottomContainer">
           <div className="walletForm">
-            walletForm
+            <div className="name">
+              name
+            </div>
+            <div className="email">
+              email
+            </div>
+            <div className="address">
+              address
+            </div>
+            <div className="wifForm">
+              <div className="wifInput">
+                wifInput
+              </div>
+              <div className="wifButton">
+                change
+              </div>
+            </div>
           </div>
         </div>
 
