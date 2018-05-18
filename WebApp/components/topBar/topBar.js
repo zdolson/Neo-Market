7556ff7c4efdc46ce71dc64ee5e96be77f3c6e90
Alec Felt <allfelt@ucsc.edu>
2018-05-17 21:23:46 -0700

    adding material-ui stuff

--- WebApp/components/topBar/topBar.js
+++ WebApp/components/topBar/topBar.js
@@ -7,6 +7,10 @@ import LogoIcon from '../assets/Logo.svg'
 import SearchIcon from '../assets/SearchIcon.svg'
 import cF from '../../neonFunctions/contractFunctions'
 import node from '../../neonFunctions/blockchain'
+
+import Popover from 'material-ui/Popover';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
 // const neon = require('@cityofzion/neon-js')
 // const Neon = neon.default
 
@@ -34,9 +38,8 @@ export class TopBar extends Component {
     this.topbarTestingButton = this.topbarTestingButton.bind(this);
     this.openFilter = this.openFilter.bind(this);
     this.closeFilter = this.closeFilter.bind(this);
-    this.titleSelect = this.titleSelect.bind(this);
-    this.descSelect = this.descSelect.bind(this);
-    this.priceSelect = this.priceSelect.bind(this);
+    this.lowToHighHandler = this.lowToHighHandler.bind(this);
+    this.highToLowHandler = this.highToLowHandler.bind(this);
     this.searchHandler = this.searchHandler.bind(this);
     this.searchChange = this.searchChange.bind(this);
     this.handleKeyPress = this.handleKeyPress.bind(this);
@@ -78,34 +81,19 @@ export class TopBar extends Component {
 
   openFilter = (event) => {
     event.preventDefault();
-    this.setState({menu_is_open: true}, () => {
-      document.addEventListener('click', this.closeFilter);
-    });
+    this.setState({menu_is_open: true});
   }
 
   closeFilter = (event) => {
-    if(!this.menu_ref.contains(event.target))
-      this.setState({menu_is_open: false}, () => {
-        document.removeEventListener('click', this.closeFilter);
-      })
+    this.setState({menu_is_open: false});
   }
 
-  titleSelect = (event) => {
-    event.preventDefault();
-    this.props.updateFilter('title');
-    this.setState( {filter_selected: 0} );
+  highToLowHandler = (e) => {
+    console.log('highToLowHandler');
   }
 
-  descSelect = (event) => {
-    event.preventDefault();
-    this.setState( {filter_selected: 1} );
-    this.props.updateFilter('description');
-  }
-
-  priceSelect = (event) => {
-    event.preventDefault();
-    this.setState( {filter_selected: 2} );
-    this.props.updateFilter('price');
+  lowToHighHandler = (e) => {
+    console.log('lowToHighHandler');
   }
 
   searchHandler = (event) => {
@@ -129,6 +117,7 @@ export class TopBar extends Component {
 
     return (
       <div className="main-container">
+
         <div className="topnav">
           <NavLink to="/" className="logo"> <LogoIcon /> </NavLink>
           <div className="search">
@@ -136,23 +125,24 @@ export class TopBar extends Component {
             <input ref="searchInput" type="text" placeholder="search..." className="searchbubble" value={this.state.search_string} onChange={this.searchChange} onKeyPress={this.handleKeyPress}></input>
             <div className="filter" onClick={this.openFilter}>
               <FilterIcon/>
+              <Popover
+                open={this.state.menu_is_open}
+                anchorEl={this.state.anchorEl}
+                anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+                targetOrigin={{horizontal: 'left', vertical: 'top'}}
+                onRequestClose={this.handleRequestClose}
+              >
+                <Menu>
+                  <MenuItem primaryText="Refresh" />
+                  <MenuItem primaryText="Help &amp; feedback" />
+                  <MenuItem primaryText="Settings" />
+                  <MenuItem primaryText="Sign out" />
+                </Menu>
+              </Popover>
             </div>
           </div>
         </div>
 
-        { this.state.menu_is_open
-          ? (
-            <div>
-              <div className="filter-flow"></div>
-              <div className="filter-menu" ref={(ref) => { this.menu_ref = ref; }}>
-                <div style={filter_styles[0]} className="filter-title" onClick={this.titleSelect}>TITLE</div>
-                <div style={filter_styles[1]} className="filter-description" onClick={this.descSelect}>DESCRIPTION</div>
-              </div>
-            </div>
-          ):(
-            null
-          )
-        }
         <Stylesheet sheet={sheet} />
       </div>
     )
